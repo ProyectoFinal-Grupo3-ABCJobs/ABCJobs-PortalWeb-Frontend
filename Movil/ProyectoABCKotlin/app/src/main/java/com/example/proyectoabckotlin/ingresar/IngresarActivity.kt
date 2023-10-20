802cc62fe5a0864ffbd0bbe5293be9dc3b0335df
Javier Leon <ja.leon1874@uniandes.edu.co>
2023-10-19 22:01:35 -0500

    UPDATE2: Crear actividad para el registro del candidato

---
+++ Movil/ProyectoABCKotlin/app/src/main/java/com/example/proyectoabckotlin/ingresar/IngresarActivity.kt
@@ -0,0 +1,12 @@
+package com.example.proyectoabckotlin.ingresar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.proyectoabckotlin.R
+
+class IngresarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingresar)
+    }
+}
