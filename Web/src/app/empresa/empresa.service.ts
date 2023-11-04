863fe3b0fe03cc02f6d3881dbc9b0b2d193e335e
SaragRam <sg.ramirez940@uniandes.edu.co>
2023-11-03 21:09:38 -0500

    Update: función service asignar empleado

--- Web/src/app/empresa/empresa.service.ts
+++ Web/src/app/empresa/empresa.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpHeaders  } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { Empresa } from './empresa';
 import { Proyecto } from './proyecto';
+import { EmpleadoInterno } from './empleadoInterno';
 import { jwtDecode } from "jwt-decode";
 
 
@@ -38,6 +39,16 @@ export class EmpresaService {
     }
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     return this.http.get<Proyecto>(`${this.backUrl}5002/company/${this.idEmpresa}/projects`, {headers})
+  }
+
+  asignarEmpleado(empleadoInterno: EmpleadoInterno): Observable<EmpleadoInterno> {
+    const token = localStorage.getItem('token'); // Obtener el token JWT de localStorage
+    if(token){  
+      const decodedToken = jwtDecode(token);
+      this.idEmpresa = decodedToken['sub']['idEmpCanFunc'];
+    }
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post<EmpleadoInterno>(`${this.backUrl}5002/company/${this.idEmpresa}/assignEmployee`,empleadoInterno, {headers})
   }
 
 
