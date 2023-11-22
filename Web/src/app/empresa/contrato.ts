f232ddd905a3f6176b377d3f621478444dc171ad
SaragRam <sg.ramirez940@uniandes.edu.co>
2023-11-22 12:32:04 -0500

    Update: modelo y servicio desempeño empleado

--- Web/src/app/empresa/contrato.ts
+++ Web/src/app/empresa/contrato.ts
@@ -1,19 +1,25 @@
 export class Contrato {
+    idContrato: number;
     numeroContrato: number;
     idCandidato: number;
+    nombreCandidato: string;
     idEmpresa: number;
     idProyecto: number;
     idCargo: number;
   
       constructor(
+        idContrato: number,
         numeroContrato: number,
         idCandidato: number,
+        nombreCandidato: string,
         idEmpresa: number,
         idProyecto: number,
         idCargo: number
       ) {
+          this.idContrato = idContrato;
           this.numeroContrato = numeroContrato;
           this.idCandidato = idCandidato;
+          this.nombreCandidato = nombreCandidato;
           this.idEmpresa = idEmpresa;
           this.idProyecto = idProyecto;
           this.idCargo = idCargo;
