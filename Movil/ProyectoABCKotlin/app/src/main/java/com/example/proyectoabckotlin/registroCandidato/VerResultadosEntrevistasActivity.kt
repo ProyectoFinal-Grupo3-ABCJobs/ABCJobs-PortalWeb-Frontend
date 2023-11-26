8300ee00aa47ed5f756a06af3e110bcb69b71de3
Jenny Idarraga <j.idarragar@uniandes.edu.co>
2023-11-26 00:51:56 -0500

    Funcionalidad ver resultados entrevistas

--- Movil/ProyectoABCKotlin/app/src/main/java/com/example/proyectoabckotlin/registroCandidato/VerResultadosEntrevistasActivity.kt
+++ Movil/ProyectoABCKotlin/app/src/main/java/com/example/proyectoabckotlin/registroCandidato/VerResultadosEntrevistasActivity.kt
@@ -1,12 +1,181 @@
 package com.example.proyectoabckotlin.registroCandidato
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.ListView
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.widget.SearchView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import com.example.proyectoabckotlin.R
+import com.example.proyectoabckotlin.databinding.ActivityRegistroResultadoPruebaBinding
+import com.example.proyectoabckotlin.databinding.ActivityVerResultadosEntrevistasBinding
+import com.example.proyectoabckotlin.pojo.Contrato
+import com.example.proyectoabckotlin.pojo.Entrevista
+import com.example.proyectoabckotlin.pojo.Prueba
+import com.example.proyectoabckotlin.service.ApiEntrevista
+import com.example.proyectoabckotlin.service.ApiPrueba
+import com.example.proyectoabckotlin.service.HeaderInterceptor
+import com.example.proyectoabckotlin.utilidades.PreferencesUtil.obtenerTokenDesdeSharedPreferences
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.Collections
+
+
 
 class VerResultadosEntrevistasActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVerResultadosEntrevistasBinding
+    private var pruebas: List<Prueba>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ver_resultados_entrevistas)
+        binding = ActivityVerResultadosEntrevistasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val apiEntrevista = getRetrofit().create(ApiEntrevista::class.java)
+        val secretKey = Keys.hmacShaKeyFor("frase-secreta-token-2023-frase-secreta-token-2023-backend-abcjobs".toByteArray())
+        val token = obtenerTokenDesdeSharedPreferences(this@VerResultadosEntrevistasActivity)
+
+        val decodedToken = Jwts.parserBuilder()
+            .setSigningKey(secretKey)
+            .setAllowedClockSkewSeconds(60) // Permitir una diferencia de hasta 60 segundos
+            .build()
+            .parseClaimsJws(token)
+
+
+        val claims = decodedToken.body
+        println("Decoded Token: ${decodedToken.body}")
+        val subClaim = claims["sub"] as Map<String, Any>
+
+
+        val idEmpCanFunc = subClaim["idEmpCanFunc"]
+        if (idEmpCanFunc != null) {
+            val call: Call<List<Entrevista>> = apiEntrevista.obtenerEntrevistasCandidato(
+                idEmpCanFunc.toString(),
+                "Bearer $token"
+            )
+            call.enqueue(object : Callback<List<Entrevista>> {
+                override fun onResponse(
+                    call: Call<List<Entrevista>>,
+                    response: Response<List<Entrevista>>
+                ) {
+                    if (response.isSuccessful) {
+                        val entrevistas: List<Entrevista>? = response.body()
+                        val cardContainer: LinearLayout = findViewById(R.id.cardContainer)
+                        cardContainer.removeAllViews()
+
+                        if (entrevistas != null) {
+                            val mensajeTextView: TextView = findViewById(R.id.mensajeTextView)
+                            mensajeTextView.visibility = View.GONE
+
+                            // Iterar sobre la lista de pruebas y crear tarjetas
+                            entrevistas.forEach { entrevista ->
+                                // Crear una instancia de CardView
+                                val cardViewLayout = layoutInflater.inflate(R.layout.cardview_layout_entrevista, null) as CardView
+
+                                val layoutParams = LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    LinearLayout.LayoutParams.WRAP_CONTENT
+                                )
+
+
+                                val marginInPixels = 12
+                                layoutParams.setMargins(marginInPixels, marginInPixels, marginInPixels, marginInPixels)
+
+
+                                cardViewLayout.layoutParams = layoutParams
+                                // Obtener las vistas del layout inflado
+                                val nombreEmpresaTextView: AppCompatTextView = cardViewLayout.findViewById(R.id.nombreEmpresaTextView)
+                                val cargoTextView: AppCompatTextView = cardViewLayout.findViewById(R.id.cargoTextView)
+                                val proyectoTextView: AppCompatTextView = cardViewLayout.findViewById(R.id.proyectoTextView)
+                                val estadoTextView: AppCompatTextView = cardViewLayout.findViewById(R.id.estadoTextView)
+                                val detallesTextView: AppCompatTextView = cardViewLayout.findViewById(R.id.detallesTextView)
+
+                                // Establecer datos en las vistas
+                                nombreEmpresaTextView.text = "${entrevista.empresaNombre ?: ""}"
+                                cargoTextView.text = "Cargo al que aplica: ${entrevista.perfilDescripcion ?: ""}"
+                                proyectoTextView.text = "Proyecto Asociado: ${entrevista.proyectoNombre ?: ""}"
+                                if(entrevista.estado==false) {
+                                    estadoTextView.text = "Estado: Pendiente"
+                                    detallesTextView.text = "Detalles: "
+                                }
+                                else {
+                                    estadoTextView.text = "Estado : Finalizada"
+                                    detallesTextView.text = "Detalles: ${entrevista.detalles ?: ""}"
+                                }
+
+
+                                val nuevoColor = Color.parseColor("#FFFFFF")
+                                cardViewLayout.setCardBackgroundColor(nuevoColor)
+
+                                // Agregar la tarjeta al contenedor
+                                cardContainer.addView(cardViewLayout)
+                            }
+
+
+                        } else {
+                            val mensajeTextView: TextView = findViewById(R.id.mensajeTextView)
+                            mensajeTextView.visibility = View.VISIBLE
+                        }
+                    } else {
+                        // Manejar la respuesta no exitosa aquí
+                    }
+                }
+
+                override fun onFailure(call: Call<List<Entrevista>>, t: Throwable) {
+                    // Manejar el fallo de la llamada aquí
+                }
+            })
+
+        }
+        try{
+            binding.regresarDesempenoButton.setOnClickListener {
+                finish()
+            }
+        } catch (ex: Exception) {
+            Toast.makeText(
+                this@VerResultadosEntrevistasActivity,
+                ex.toString(),
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+    }
+
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("http://LoadBalancerProyectoABC-735612126.us-east-2.elb.amazonaws.com:5003/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(this.getClient())
+            .build()
+    }
+
+    private fun getClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient.Builder()
+            .protocols(Collections.singletonList(Protocol.HTTP_1_1))
+            .addInterceptor(HeaderInterceptor())
+            .addInterceptor(loggingInterceptor)
+            .build()
     }
 }
--- Movil/ProyectoABCKotlin/app/src/main/java/com/example/proyectoabckotlin/service/ApiEntrevista.kt
+++ Movil/ProyectoABCKotlin/app/src/main/java/com/example/proyectoabckotlin/service/ApiEntrevista.kt
@@ -1,18 +1,19 @@
 package com.example.proyectoabckotlin.service
 
-import com.example.proyectoabckotlin.pojo.Contrato
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.example.proyectoabckotlin.pojo.Entrevista
 import com.example.proyectoabckotlin.pojo.Prueba
 import retrofit2.Call
-import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.POST
 import retrofit2.http.Path
 
-interface ApiPrueba {
-    @GET("/test/candidate/{idCandidato}")
-    fun obtenerPruebasCandidato(
+interface ApiEntrevista {
+    @GET("/test/candidate/{idCandidato}/interviews")
+    fun obtenerEntrevistasCandidato(
         @Path("idCandidato") idCandidato: String,
         @Header("Authorization") authorization: String
-    ): Call<List<Prueba>>
+    ): Call<List<Entrevista>>
 }
